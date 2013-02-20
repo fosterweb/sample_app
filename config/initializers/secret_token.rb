d2b204ba2fc402ccb3cd53e02eec7cfaa616f39c
fosterweb <paul.decle@fosterweb.co.uk>
2013-02-20 21:51:04 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f6e17cbac86e0d771293cb7dbc1fd0ad27fbaf71367bdc5517df16c9078704db7d0776f834c5ad33c925bb787cfa556765d9b3e965a00f9d45872ab1bff21d66'
